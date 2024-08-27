5bfb16a92e8aa01e77d190e72aef9e481bd8ca79
Luiz Kim <luiz.kim@controleonline.com>
2024-08-27 16:16:37 -0300

    Update

--- src/pages/Provider/Details.vue
+++ src/pages/Provider/Details.vue
@@ -1,5 +1,5 @@
 <template>
-  <People :context="context" v-if="context"/>
+  <People :context="context" :peopleId="peopleId" v-if="context" />
 </template>
 
 <script>
@@ -7,15 +7,17 @@ import People from "@controleonline/ui-people/src/components/People/Details.vue"
 
 export default {
   components: {
-      People,
+    People,
   },
 
   data() {
     return {
-      context: 'provider'
+      context: "provider",
+      peopleId: null,
     };
   },
   created() {
-   },
+    this.peopleId = decodeURIComponent(this.$route.params.id);
+  },
 };
 </script>
