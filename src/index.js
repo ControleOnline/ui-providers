888d04ec63322b801d01e9270d4a9ac1489c66c5
Luiz Kim <luiz.kim@controleonline.com>
2026-09-06 12:34:05 -0300

    test: document ui-providers people smoke flow

--- src/index.js
+++ src/index.js
@@ -2,3 +2,4 @@
 const moduleEntry = {};
 
 export default moduleEntry;
+// fluxo: fornecedor-cadastro | etapa: provider-module-entry | wiki: https://github.com/ControleOnline/app-community/wiki/Venda-Producao
